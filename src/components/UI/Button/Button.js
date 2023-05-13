19a51db2d6614c120bbd7544a98d2661f87143f6
rezaBG <rezabarzegargashti@gmail.com>
2023-05-13 14:54:39 +0200

    add button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,6 +2,17 @@ import React from "react";
 
 import classes from "./Button.module.css";
 
-const Button = () => {};
+const Button = (props) => {
+  return (
+    <button
+      type={props.type || "button"}
+      className={`${classes.button} ${props.className} `}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
